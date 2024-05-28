7eb3beffe29be7d3409803f5fd010d96989b7b9a
EnsarKrehmic <krehmiicjr@gmail.com>
2024-05-28 16:38:11 +0200

    Ispravke na backendu vezane za organizaciju i povezivanje fajlova

--- frizerski-salon/backend/index.js
+++ frizerski-salon/backend/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const userRoutes = require('./routes/users');
 const cors = require('cors');
 const path = require('path');
@@ -12,10 +11,10 @@ app.use(cors());
 app.use(express.json());
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
 
 // API routes
-app.get('/api/data', (req, res) => {
+app.use('/api/data', (req, res) => {
     res.json([
         { id: 1, name: 'Item 1' },
         { id: 2, name: 'Item 2' },
@@ -23,12 +22,17 @@ app.get('/api/data', (req, res) => {
     ]);
 });
 
-app.use(bodyParser.json());
 app.use('/api/users', userRoutes);
 
 // The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
 
 const PORT = process.env.PORT || 3000;
