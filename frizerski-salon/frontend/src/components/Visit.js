265605bbfa0d35be3dcd51bb6b365201e6a281fb
EnsarKrehmic <krehmiicjr@gmail.com>
2024-06-01 01:40:16 +0200

    Dodana i uređena opcija Termin

---
+++ frizerski-salon/frontend/src/components/Visit.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Visit.css';
+
+function Visit() {
+  return (
+    <section class="visit" id="visit">
+        <h1 class="headingtermin"> Zakažite Termin </h1>
+        <div class="row">
+            <form action="">
+                <h3>Zakažite Vaš Prvi Termin Danas</h3>
+                <div class="inputBox">
+                    <input type="text" placeholder="Vaše Ime"/>
+                </div>
+                <div class="inputBox">
+                    <input type="email" placeholder="Vaš E-mail"/>
+                </div>
+                <div class="inputBox">
+                    <input type="text" placeholder="Predmet"/>
+                </div>
+                <div class="inputBox">
+                    <input type="text" placeholder="Poruka"/>
+                </div>
+                <input type="submit" value="POŠALJI" class="btn"/>
+            </form>
+            <div class="image">
+                <img src="images/contact.png" alt=""/>
+            </div>
+        </div>
+    </section>
+  );
+}
+
+export default Visit;
