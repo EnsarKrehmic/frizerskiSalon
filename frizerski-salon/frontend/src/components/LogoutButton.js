edd5e65e77c14cc829e0672d7d42c4d528362a8d
EnsarKrehmic <krehmiicjr@gmail.com>
2024-06-02 21:49:23 +0200

    Dodan logout button

---
+++ frizerski-salon/frontend/src/components/LogoutButton.js
@@ -0,0 +1,24 @@
+import React from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+function LogoutButton(){
+
+    const navigate=useNavigate();
+
+    const handleLogout=()=>{
+        try {
+            axios.get('http://localhost:3307/logout');
+            console.log('Uspješno ste se odjavili.');
+            navigate("/");
+        } catch (error) {
+            console.error('Odjava neuspješna:', error.message);
+        }
+    }
+
+    return(
+        <button onClick={handleLogout} className="btn btn-danger m-2">Odjava</button>
+    )
+}
+
+export default LogoutButton;
