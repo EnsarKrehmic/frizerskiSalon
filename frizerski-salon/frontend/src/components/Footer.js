3fec147ae672f96ca80dcc0d69405b3f2268d702
EnsarKrehmic <krehmiicjr@gmail.com>
2024-06-11 00:00:11 +0200

    Postavljeni putevi do određenih formi

--- frizerski-salon/frontend/src/components/Footer.js
+++ frizerski-salon/frontend/src/components/Footer.js
@@ -2,27 +2,39 @@ import React from 'react';
 import './Footer.css';
 import { Link } from "react-router-dom";
 
-function Footer() {
+const Footer = () => {
   return (
     <section className="footer" id="footer">
         <div className="box-container">
             <div className="box">
-            <h3>Pronađite Nas Ovdje</h3>
+                <h3>Pronađite Nas Ovdje</h3>
                 <p>
-                    Možete nas posetiti direktno na našoj <Link to="/contact" style={{ color: 'blue', textDecoration: 'underline' }}>lokaciji</Link>.
+                    Možete nas posjetiti direktno na našoj <Link to="/contact" style={{ color: 'blue', textDecoration: 'underline' }}>lokaciji</Link>.
                 </p>
             </div>
             <div className="box">
                 <h3>Kontaktirajte Nas</h3>
                 <p>+62 225 883</p>
-                <a href="#" className="link">krehmiicjr@gmail.com</a>
+                <p>
+                  <button onClick={() => window.location = 'mailto:ensar.krehmic.22@size.ba'} className="link-button">
+                    ensar.krehmic.22@size.ba
+                  </button>
+                  <br></br>
+                  <button onClick={() => window.location = 'mailto:kemal.muminovic.22@size.ba'} className="link-button">
+                    kemal.muminovic.22@size.ba
+                  </button>
+                  <br></br>
+                  <button onClick={() => window.location = 'mailto:harun.smriko.22@size.ba'} className="link-button">
+                    harun.smriko.22@size.ba
+                  </button>
+                </p>
             </div>
             <div className="box">
                 <h3>Lokacija</h3>
                 <p>
-                  Hase Bega Huseinovića bb <br></br>
-                  Nemila <br></br>
-                  Bosna i Hercegovina <br></br>
+                  Studentski centar Zenica<br />
+                  Crkvice 50, Zenica 72000 <br />
+                  Bosna i Hercegovina <br />
                 </p>
             </div>
         </div>
