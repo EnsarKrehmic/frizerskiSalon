4f2deab12e13658ae39f64fc339c95232372d944
EnsarKrehmic <krehmiicjr@gmail.com>
2024-06-11 00:14:41 +0200

    Dodan UserProvider u okvir App komponente

--- frizerski-salon/frontend/src/index.js
+++ frizerski-salon/frontend/src/index.js
@@ -1,5 +1,6 @@
+import { UserProvider } from './components/UserContext';
 import { createRoot } from 'react-dom/client';
-import { React, StrictMode } from 'react';
+import { StrictMode } from 'react';
 
 import reportWebVitals from './reportWebVitals';
 import App from './components/App';
@@ -11,9 +12,11 @@ const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
+  <StrictMode>
+    <UserProvider>
+      <App />
+    </UserProvider>
+  </StrictMode>
 );
 
 reportWebVitals();
