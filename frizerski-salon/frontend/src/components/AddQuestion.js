bbed45849f9767162b5d71388845859f9c129e28
EnsarKrehmic <krehmiicjr@gmail.com>
2024-06-11 00:12:35 +0200

    Popravljena funkcionalnost postavljanja pitanja radniku

--- frizerski-salon/frontend/src/components/AddQuestion.js
+++ frizerski-salon/frontend/src/components/AddQuestion.js
@@ -2,36 +2,35 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
 
-function AddQuestion(){
-    const {id}=useParams();
+function AddQuestion() {
+    const { id } = useParams();
     const [userInfo, setUserInfo] = useState(null);
-    const [values, setValues]=useState({
-        content:'',
-        user_id:''
-    })
+    const [values, setValues] = useState({
+        content: '',
+        user_id: ''
+    });
 
     axios.defaults.withCredentials = true;
     const navigate = useNavigate();
 
     const handleInput = (event) => {
-        setValues(prev => ({ ...prev, [event.target.name]: event.target.value }))
+        setValues(prev => ({ ...prev, [event.target.name]: event.target.value }));
     };
 
-    const handleSubmit=(event)=>{
-        values.user_id=userInfo.id;
+    const handleSubmit = (event) => {
         event.preventDefault();
-        axios.post("http://localhost:3307/add-question/"+id, values)
-        .then(res=>{
-            if (res.data==="OK"){
-                alert("Pitanje uspješno postavljeno.");
-                navigate(`/chosen-travel/${id}`);
-            }
-            else{
-                alert("Pitanje nije postavljeno.");
-            }
-        })
-        .catch(err=>console.log(err));
-    }
+        values.user_id = userInfo.id;
+        axios.post(`http://localhost:3307/add-question/${id}`, values)
+            .then(res => {
+                if (res.data === "OK") {
+                    alert("Pitanje uspješno postavljeno.");
+                    navigate(`/chosen-worker/${id}`);
+                } else {
+                    alert("Pitanje nije postavljeno.");
+                }
+            })
+            .catch(err => console.log(err));
+    };
 
     useEffect(() => {
         axios.get("http://localhost:3307/api/user")
@@ -43,22 +42,22 @@ function AddQuestion(){
         return <div>Loading...</div>;
     }
 
-    return(
+    return (
         <div className="d-flex justify-content-center align-items-center bg-warning vh-100">
             <div className="bg-white p-3 rounded w-25">
                 <h2>Postavite pitanje</h2>
-                <Link to={`/chosen-worker/${id}`} className="btn btn-default border w-100 bg-light text-decoration-none">Nazad</Link>
-                <form action="" onSubmit={handleSubmit}>
+                <Link to={`/chosen-worker/${id}`} className="btn btn-success border w-100 bg-dark text-decoration-none">Nazad</Link>
+                <form onSubmit={handleSubmit}>
                     <div className="mb-3">
                         <label htmlFor="content">Sadržaj</label>
                         <input type="text" placeholder="Unesite sadržaj" name="content"
-                        onChange={handleInput} className="form-control rounded-0"/>
+                            onChange={handleInput} className="form-control rounded-0" />
                     </div>
                     <button type="submit" className="btn btn-success w-100 mb-2">Postavite pitanje</button>
                 </form>
             </div>
         </div>
-    )
+    );
 }
 
 export default AddQuestion;
