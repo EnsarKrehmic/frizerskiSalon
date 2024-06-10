43927b96a8c0b4ef27c2ccc0069fa5443f9098cf
EnsarKrehmic <krehmiicjr@gmail.com>
2024-06-11 00:05:29 +0200

    Uređen tekst recenzija

--- frizerski-salon/frontend/src/components/Review.js
+++ frizerski-salon/frontend/src/components/Review.js
@@ -9,19 +9,22 @@ function Review() {
     <section className="review" id="review">
       <h1 className="heading">Recenzije</h1>
       <div className="box-container">
-        <div className="box">
-          <img src={pic1} alt="Client 1" />
-          <h3>Client 1</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
+        
         <div className="box">
           <img src={pic2} alt="Client 2" />
-          <h3>Client 2</h3>
+          <h3>Ronaldo</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
+
+        <div className="box">
+          <img src={pic1} alt="Client 1" />
+          <h3>Messi</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+
         <div className="box">
           <img src={pic3} alt="Client 3" />
-          <h3>Client 3</h3>
+          <h3>Ronaldo</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
       </div>
